3c13efb16e1c0a96b26456853c7aaa40e774f392
srxo <salmaramdani01@gmail.com>
2024-10-30 16:04:17 +0100

    db creatie voor het aanmaken van kledingstukken en migratie, user validatie dmv middleware

--- _changelog/changelog.md
+++ _changelog/changelog.md
@@ -5,3 +5,7 @@ ik heb een **tekst**
 [naam van de link (www.google.com)]
 
 ![alt text(./images/naamplaatje)]
+
+# 10/16/2024 
+- database migration
+
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,10 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Tests\Fixtures\Controllers\UserController;
+use App\Http\Controllers\ClothesController;
 
 Route::get('/', function () {
-    //test
     return view('welcome');
 });
 
@@ -12,16 +13,24 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware(['auth', 'role:user'])->group(function () {
+    Route::get('/user-area', [UserController::class, 'index']);
+});
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+// Voeg middleware toe voor de clothing routes
+Route::middleware('auth')->group(function () {
+    Route::get('/clothes/create', [ClothesController::class, 'create'])->name('clothes.create');
+    Route::post('/clothes', [ClothesController::class, 'store'])->name('clothes.store');
+});
+
 Route::get('/about-us', function() {
     return 'This page is about us';
 });
-
-
 
 require __DIR__.'/auth.php';
